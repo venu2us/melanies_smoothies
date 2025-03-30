b90a5f4245c274e1b1c9a5b7850b6862905d5150
venu2us <studyupskill@gmail.com>
2025-03-29 18:47:46 -0700

    Update streamlit_app.py
    code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,6 @@ if ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-   
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)   
