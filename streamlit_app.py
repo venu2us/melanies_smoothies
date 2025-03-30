1aca426ec463775b7db84aef2768066473683662
venu2us <studyupskill@gmail.com>
2025-03-29 17:59:09 -0700

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,41 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
+#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-from snowflake.snowpark.functions import col 
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your smoothie will be ", name_on_order)
 
-session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')) 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:'
     , my_dataframe
+    , max_selections=5
 )
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
+   ingredients_string = ''
 
-    ingredients_string = ''
-
-    for fruit_chosen in ingredients_list:
+for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
 
-    st.write(ingredients_string)
+        st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
 st.write(my_insert_stmt)
+#st.stop()
 
 if ingredients_string:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
+
+   
