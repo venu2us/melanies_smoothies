2c7398ddd82537cfa70f2a014932e56575e13d1e
venu2us <studyupskill@gmail.com>
2025-03-29 22:07:57 -0700

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,12 @@ for fruit_chosen in ingredients_list:
 
         st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+my_insert_stmt = """ 
+            insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')
+        """
 
-st.write(my_insert_stmt)
+st.write("SQL Query", my_insert_stmt)
 #st.stop()
 
 if ingredients_string:
